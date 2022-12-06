14170c8bd63e2f65ffe32ac215fcc4d2cfb702fd
rahalh <hamzarahal031@gmail.com>
2022-12-06 20:49:14 +0100

    solves problem3

---
+++ problem3_part1/Program.cs
@@ -0,0 +1,33 @@
+﻿using var reader = new StreamReader("input.txt");
+
+var sum = 0;
+
+while (true)
+{
+    var line = reader.ReadLine();
+    if (line == null)
+    {
+        Console.Write(sum);
+        return 0;
+    }
+
+    var comp1 = line.Take(line.Length / 2).ToHashSet();
+    var comp2 = line.TakeLast(line.Length / 2).ToHashSet();
+    var intersection = comp1.Intersect(comp2);
+    sum += calculatePriorities(intersection);
+}
+
+int calculatePriorities(IEnumerable<char> set)
+{
+    var sum = 0;
+    foreach (var c in set)
+    {
+        if ((int)c >= 97)
+        {
+            sum += (int)c - 96;
+            continue;
+        }
+        sum += (int)c - 38;
+    }
+    return sum;
+}
---
+++ problem3_part2/Program.cs
@@ -0,0 +1,34 @@
+﻿using var reader = new StreamReader("input.txt");
+
+var sum = 0;
+
+while (true)
+{
+    var line1 = reader.ReadLine();
+    if (line1 == null)
+    {
+        Console.Write(sum);
+        return 0;
+    }
+
+    var line2 = reader.ReadLine();
+    var line3 = reader.ReadLine();
+
+    var intersection = line1.Intersect(line2).Intersect(line3);
+    sum += calculatePriorities(intersection);
+}
+
+int calculatePriorities(IEnumerable<char> set)
+{
+    var sum = 0;
+    foreach (var c in set)
+    {
+        if ((int)c >= 97)
+        {
+            sum += (int)c - 96;
+            continue;
+        }
+        sum += (int)c - 38;
+    }
+    return sum;
+}
